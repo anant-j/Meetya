c684d39c21c2762e73d9df068a8ad5910d498f86
ArmanGolmakan <arman_g75@yahoo.com>
2018-11-24 04:25:12 -0500

    fixed the array return type

--- add.js
+++ add.js
@@ -1,8 +1,10 @@
 
-var interests = [];
+var interests = []; //this array will hold all interest items
 
 function myFunction() {
     var x = document.getElementById("myText").value;
     interests.push(x);
     document.getElementById('demo').innerHTML = interests;
+    return interests;
 }
+
